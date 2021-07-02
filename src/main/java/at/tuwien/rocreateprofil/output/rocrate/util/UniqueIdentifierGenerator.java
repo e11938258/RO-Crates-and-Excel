d2e108bbde4fa1d82c313eab8604c40e4b5789d9
marwin <martin.winkler@gmail.com>
2021-07-02 23:07:37 +0200

    * Added random uuid suffix generation for possibly colliding sheet/feature names

--- src/main/java/at/tuwien/rocreateprofil/output/rocrate/util/UniqueIdentifierGenerator.java
+++ src/main/java/at/tuwien/rocreateprofil/output/rocrate/util/UniqueIdentifierGenerator.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 public class UniqueIdentifierGenerator {
 
     private static final String LOCAL_RESOLUTION_IDENTIFIER = "#";
+    private static final String PREFIX_SUFFIX_SEPARATOR = "-";
+    private static final int DEFAULT_SUFFIX_LENGTH = 4;
 
     private static final HashMap<String, String> uuidsInUse = new HashMap<>();
 
@@ -19,8 +21,26 @@ public class UniqueIdentifierGenerator {
         return notYetUsedUuid;
     }
 
+    public static synchronized String generateUniqueUUIDWithPrefix(String prefix) {
+        return generateUniqueUUIDWithPrefix(prefix, DEFAULT_SUFFIX_LENGTH);
+    }
+
+    public static synchronized String generateUniqueUUIDWithPrefix(String prefix, Integer randomSuffixLength) {
+        String notYetUsedUuid = prefix + PREFIX_SUFFIX_SEPARATOR + generateRandomUuid(randomSuffixLength);
+        while (uuidsInUse.containsKey(notYetUsedUuid)) {
+            notYetUsedUuid = prefix + PREFIX_SUFFIX_SEPARATOR + generateRandomUuid(randomSuffixLength);
+        }
+        // prepend with # to indicate that the reference is within Ro-Crate
+        notYetUsedUuid = LOCAL_RESOLUTION_IDENTIFIER + notYetUsedUuid;
+        return notYetUsedUuid;
+    }
+
     private static String generateRandomUuid() {
         UUID newUuid = UUID.randomUUID();
         return newUuid.toString();
+    }
+
+    private static String generateRandomUuid(int length) {
+        return generateRandomUuid().substring(0, length);
     }
 }
